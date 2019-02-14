990185d240842adaead5db2eabf0a38986f29028
Dustin Yost <dustin.yost@mymail.champlain.edu>
2019-02-14 16:01:55 -0500

    Add toolbar plugin to see how default unreal plugins handle extensions

---
+++ Plugins/ToolbarPlugin/Source/ToolbarPlugin/Private/ToolbarPluginCommands.cpp
@@ -0,0 +1,12 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "ToolbarPluginCommands.h"
+
+#define LOCTEXT_NAMESPACE "FToolbarPluginModule"
+
+void FToolbarPluginCommands::RegisterCommands()
+{
+	UI_COMMAND(PluginAction, "ToolbarPlugin", "Execute ToolbarPlugin action", EUserInterfaceActionType::Button, FInputGesture());
+}
+
+#undef LOCTEXT_NAMESPACE
